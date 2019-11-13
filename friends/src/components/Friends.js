b71a641e403f2203b8fe8add9b60098f6b592866
ngaivoronski <ngaivoronski@gmail.com>
2019-11-13 13:39:47 -0500

    edit, delete, and logout functionality added

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import { Redirect } from "react-router-dom";
 
 export default function Friends() {
     const [friends, setFriends] = useState([]);
@@ -8,6 +9,8 @@ export default function Friends() {
         age: "",
         email: "",
     })
+    const [editing, setEditing] = useState("");
+    const [loggedIn, setLoggedIn] = useState(false);
 
     const formChange = event => {
         setNewFriend({
@@ -17,21 +20,68 @@ export default function Friends() {
     }
 
     const submitForm = event => {
-        event.preventDefault();
+        if (editing === "") {
+            event.preventDefault();
+            axios
+            .post("http://localhost:5000/api/friends", newFriend, { headers:{authorization: sessionStorage.getItem("token")}})
+            .then(response => {
+                console.log(response);
+                setFriends(response.data);
+                setNewFriend({
+                    name: "",
+                    age: "",
+                    email: "",
+                })
+            })
+            .catch(err => {
+                console.log("there was an error", err)
+            });
+        } else {
+            event.preventDefault();
+            axios
+            .put(`http://localhost:5000/api/friends/${editing}`, newFriend, { headers:{authorization: sessionStorage.getItem("token")}})
+            .then(response => {
+                console.log(response);
+                setFriends(response.data);
+                setNewFriend({
+                    name: "",
+                    age: "",
+                    email: "",
+                })
+                setEditing("");
+            })
+            .catch(err => {
+                console.log("there was an error", err)
+            });
+        }
+    }
+
+    const deleteFriend = id => {
         axios
-        .post("http://localhost:5000/api/friends", newFriend, { headers:{authorization: sessionStorage.getItem("token")}})
+        .delete(`http://localhost:5000/api/friends/${id}`, { headers:{authorization: sessionStorage.getItem("token")}})
         .then(response => {
             console.log(response);
             setFriends(response.data);
-            setNewFriend({
-                name: "",
-                age: "",
-                email: "",
-            })
         })
         .catch(err => {
             console.log("there was an error", err)
         });
+    }
+
+    const editFriend = person => {
+        setEditing(person.id);
+        setNewFriend({
+            name: person.name,
+            age: person.age,
+            email: person.email,
+        });
+    }
+
+    const LogOut = () => {
+        sessionStorage.removeItem("token");
+        console.log("Logout fires!");
+        console.log(sessionStorage);
+        setLoggedIn(false);
     }
 
     useEffect(() => {
@@ -44,28 +94,45 @@ export default function Friends() {
         .catch(err => {
             console.log("there was an error", err)
         });
-    },[]);
-    return (
-        <div className="friends-div">
-            <form className="friend-form-div" onSubmit={submitForm}>
-                <label htmlFor="name">Name:</label>
-                <input name="name" value={newFriend.name} onChange={formChange}/>
-                <label htmlFor="age">Age:</label>
-                <input name="age" value={newFriend.age} onChange={formChange}/>
-                <label htmlFor="email">Email:</label>
-                <input name="email" value={newFriend.email} onChange={formChange}/>
-                <button type="submit">Add New Friend</button>
-            </form>
-            <div className="friend-list-div">
-                {friends.map(person => (
-                    <div key={person.id}>
-                        <h2>{person.name}</h2>
-                        <p>{person.age}</p>
-                        <p>{person.email}</p>
-                    </div>
-                ))}
+    },[loggedIn]);
+
+    useEffect(() => {
+        if (sessionStorage.getItem("token")) {
+        setLoggedIn(true);
+        } else {
+        setLoggedIn(false);
+        }
+    },[])
+
+    if (!sessionStorage.getItem("token")) {
+        console.log("rerender");
+        return( <Redirect to="/login"/> );
+    } else {
+        return (
+            <div className="friends-div">
+                <form className="friend-form-div" onSubmit={submitForm}>
+                    <label htmlFor="name">Name:</label>
+                    <input name="name" value={newFriend.name} onChange={formChange}/>
+                    <label htmlFor="age">Age:</label>
+                    <input name="age" value={newFriend.age} onChange={formChange}/>
+                    <label htmlFor="email">Email:</label>
+                    <input name="email" value={newFriend.email} onChange={formChange}/>
+                    <button type="submit">{editing === "" ? "Add New Friend" : "Edit Friend"}</button>
+                </form>
+                <div className="friend-list-div">
+                    {friends.map(person => (
+                        <div key={person.id}>
+                            <h2>{person.name}</h2>
+                            <p>{person.age}</p>
+                            <p>{person.email}</p>
+                            <button onClick={() => editFriend(person)}>Edit</button>
+                            <button onClick={() => deleteFriend(person.id)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
+                <button onClick={LogOut}>Log Out</button>
             </div>
-        </div>
-        
-    )
+            
+        )
+    }
 }
